5f1e9c4788cb8a246d0343fb004d1065be447e48
roiesh <roieshalom@users.noreply.github.com>
2025-03-12 18:30:53 +0100

    Updated site with latest fixes

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/BETU/", // ✅ Ensure this matches your GitHub repo name exactly
+  base: "/BETU/", // ✅ Ensure this matches your GitHub repo name EXACTLY
 });
-
