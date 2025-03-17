fd38f1576279bc4a29ec57d0439fee85f7cae3a3
roiesh <roieshalom@users.noreply.github.com>
2025-03-17 14:26:53 +0100

    Restored questions.js and ensured latest updates

--- questions.js
+++ questions.js
@@ -1,31 +1,32 @@
 export const questions = [
-  {
-      id: 1,
-      question: "Wie heißt die deutsche Verfassung?",
-      options: ["Volksgesetz", "Bundesgesetz", "Deutsches Gesetz", "Grundgesetz"],
-      correct_answer: "Grundgesetz"
-  },
-  {
-      id: 2,
-      question: "Wahlen in Deutschland sind frei. Was bedeutet das?",
-      options: [
-          "Man darf Geld annehmen, wenn man dafür eine bestimmte Partei wählt.",
-          "Nur Personen, die noch nie im Gefängnis waren, dürfen wählen.",
-          "Die Wählerin/der Wähler darf bei der Wahl weder beeinflusst noch gezwungen werden.",
-          "Alle wahlberechtigten Personen müssen wählen."
-      ],
-      correct_answer: "Die Wählerin/der Wähler darf bei der Wahl weder beeinflusst noch gezwungen werden."
-  },
-  {
-      id: 3,
-      question: "Deutschland ist ein Rechtsstaat. Was bedeutet das?",
-      options: [
-          "Alle Einwohner/Einwohnerinnen und der Staat müssen sich an die Gesetze halten.",
-          "Der Staat muss sich nicht an die Gesetze halten.",
-          "Nur Deutsche müssen die Gesetze befolgen.",
-          "Die Gerichte machen die Gesetze."
-      ],
-      correct_answer: "Alle Einwohner/Einwohnerinnen und der Staat müssen sich an die Gesetze halten."
-  }
-  // Add the remaining 307 questions here
-];
+    {
+        id: 1,
+        question: "Wie heißt die deutsche Verfassung?",
+        options: ["Volksgesetz", "Bundesgesetz", "Deutsches Gesetz", "Grundgesetz"],
+        correct_answer: "Grundgesetz"
+    },
+    {
+        id: 2,
+        question: "Wahlen in Deutschland sind frei. Was bedeutet das?",
+        options: [
+            "Man darf Geld annehmen, wenn man dafür eine bestimmte Partei wählt.",
+            "Nur Personen, die noch nie im Gefängnis waren, dürfen wählen.",
+            "Die Wählerin/der Wähler darf bei der Wahl weder beeinflusst noch gezwungen werden.",
+            "Alle wahlberechtigten Personen müssen wählen."
+        ],
+        correct_answer: "Die Wählerin/der Wähler darf bei der Wahl weder beeinflusst noch gezwungen werden."
+    },
+    {
+        id: 3,
+        question: "Deutschland ist ein Rechtsstaat. Was bedeutet das?",
+        options: [
+            "Alle Einwohner/Einwohnerinnen und der Staat müssen sich an die Gesetze halten.",
+            "Der Staat muss sich nicht an die Gesetze halten.",
+            "Nur Deutsche müssen die Gesetze befolgen.",
+            "Die Gerichte machen die Gesetze."
+        ],
+        correct_answer: "Alle Einwohner/Einwohnerinnen und der Staat müssen sich an die Gesetze halten."
+    }
+    // Add more questions here if needed
+  ];
+  
