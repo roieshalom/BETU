e434f7bcb585c6e598da63481d0be3cdab1163ec
roiesh <roieshalom@users.noreply.github.com>
2025-03-19 20:10:38 +0100

    Renamed project to Fragenkatalog

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/BETU/",  // ✅ This must match your repo name exactly!
+  base: "/fragenkatalog/",  // ✅ This must match your repo name exactly!
   plugins: [react()],
   server: {
     port: 5173,   // Keeps localhost fixed
